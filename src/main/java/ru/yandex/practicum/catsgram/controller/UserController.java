f697e78cdb8f22bfb1bc70f35342d89607e558b5
LUKIDMI <i@lukidmi.ru>
2026-01-05 18:12:36 +0300

    Доработан запрос на создание юзера Добавлено исключение для дубликатов Добавлен генератор идентификатора

--- src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
@@ -4,8 +4,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.yandex.practicum.catsgram.exception.ConditionsNotMetException;
+import ru.yandex.practicum.catsgram.exception.DuplicatedDataException;
 import ru.yandex.practicum.catsgram.model.User;
 
+import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -19,9 +21,26 @@ public class UserController {
         if(user.getEmail().isBlank()){
             throw new ConditionsNotMetException("Имейл должен быть указан");
         }
-
+        if(isDuplicateEmail(user.getEmail())){
+            throw new DuplicatedDataException("Этот имейл уже используется");
+        }
+        user.setId(getNextId());
+        user.setRegistrationDate(Instant.now());
+        users.put(user.getId(), user);
         return user;
     }
 
+    private boolean isDuplicateEmail(String email){
+        return users.values().stream().anyMatch(user -> user.getEmail().equals(email));
+    }
+
+    private long getNextId() {
+        long currentMaxId = users.keySet()
+                .stream()
+                .mapToLong(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
 
 }
